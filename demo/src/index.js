66c8d24c62182d3b0490c969e1e5572d4273ab18
Carl Kwok <carlkwokcy@gmail.com>
2020-12-09 16:35:27 +0800

    Fix: portrait photo display

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,7 +5,7 @@ import FbImageLibrary from '../../src'
 
 const images = ['https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350',
     'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
-    'https://wallpaperbrowse.com/media/images/soap-bubble-1958650_960_720.jpg',
+    'https://images.unsplash.com/photo-1607435655201-1c8db5afc449?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=arun-clarke-wA5-md9oGpc-unsplash.jpg',
     'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg',
     'https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350',
     "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
